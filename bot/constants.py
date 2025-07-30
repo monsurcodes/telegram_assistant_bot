75bba53f6026a5d03fee1e01217cf977e611fb02
monsurcodes <monsurcodes@gmail.com>
2025-07-31 01:06:01 +0530

    Add START_MESSAGE and OWNER_START_MESSAGE constants for personalized greetings

--- bot/constants.py
+++ bot/constants.py
@@ -2,3 +2,6 @@
 COMMAND_PREFIX = "/"
 
 WELCOME_MESSAGE = "Welcome to the group! Feel free to ask for help with /help."
+
+START_MESSAGE = "Hey! {}.\nThis is personal AssistantBot of @minkxx69\nHow may I help you today?"
+OWNER_START_MESSAGE = "Hey! Boss {}.\nI am your personal AssistantBot.\nHow may I help you?"
