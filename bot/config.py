6e36094491fbc54e2d7e8fe0f50a63c90f2f3bf1
monsurcodes <monsurcodes@gmail.com>
2025-07-31 15:11:03 +0530

    Refactor config.py:
    Organize environment variable loading and improve code structure

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,6 +8,9 @@ API_ID = int(os.getenv("API_ID", "123456"))
 API_HASH = os.getenv("API_HASH", "your_api_hash_here")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
 OWNER_ID = int(os.getenv("OWNER_ID"))
+
+DISABLED_PLUGINS = os.getenv("DISABLED_PLUGINS").strip().split(",")
+HELP_DISABLED_PLUGINS = os.getenv("HELP_DISABLED_PLUGINS").strip().split(",")
 
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
 MONGODB_DBNAME = os.getenv("MONGODB_DBNAME", "telegram_assistant_bot")
